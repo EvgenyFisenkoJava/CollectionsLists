a48f0d700c5990f22d7ae275dbc11dfaa101eab7
Evgeny Fisenko <evgeny.f.aircelle@gmail.com>
2022-06-23 09:05:27 +1000

    Initial commit

---
+++ src/main/java/pro/sky/java/collectionslists/CollectionsListsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.collectionslists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsListsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsListsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/collectionslists/CollectionsListsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.collectionslists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsListsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
